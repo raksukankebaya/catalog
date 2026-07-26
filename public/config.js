b596f3874a6d6f7f3f42c5ee58c4676a09baf80b
raksukankebaya <raksukankebaya@gmail.com>
2026-07-27 00:56:34 +0700

    Update config.js

--- public/config.js
+++ public/config.js
@@ -1,3 +1,3 @@
 window.RAKSUKAN_CONFIG = {
-  apiUrl: "https://script.google.com/macros/s/AKfycbxAQRrDkMVjiYUCkmE0V3des-edVKEPFIfwhIcIca8mHehq_uLImDMr5xsfxQbaMX2h/exec"
+  apiUrl: "https://script.google.com/macros/s/AKfycbzeC638dtv-yVfdi1CdhZFr9vOOqxp4GDMU3h5Ou1oyUmApI6OQBy7Zr6EY_Ow6drDU/exec"
 };
